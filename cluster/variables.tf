11eb6b032bfbf6c45e61b8407eb7c1cb473b2304
leandrod <leandro.e.dal.bo@gmail.com>
2024-11-26 20:11:17 +0000

    started with the cluster

---
+++ cluster/variables.tf
@@ -0,0 +1,16 @@
+variable "env" {
+  type = string
+
+}
+
+variable "region" {
+  type = string
+}
+
+variable "state_bucket" {
+  type = string
+}
+
+variable "state_bucket_key" {
+  type = string
+}
