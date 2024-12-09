8cc1b1367053a7cb011f75435e6f3e4829d7cfbc
leandrod <leandro.e.dal.bo@gmail.com>
2024-12-09 16:16:03 +0000

    adding lambda role2

--- outputs.tf
+++ outputs.tf
@@ -71,6 +71,15 @@ output "cluster_role_arn" {
   value = aws_iam_role.cluster_role.arn
 }
 
+output "lambda_role_name" {
+  value = aws_iam_role.lambda_role.name
+}
+
+output "lambda_role_arn" {
+  value = aws_iam_role.lambda_role.arn
+}
+
+
 output "alb_sg_id" {
   value     = aws_security_group.albsg.id
   sensitive = true
